ee6c1c8ec3ad2d92ccae61ede1ec97d45dc72635
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-17 22:35:22 +0200

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const body = document.querySelector("body");
+const btnStart = document.querySelector("button[data-start]");
+const btnStop = document.querySelector("button[data-stop]");
 
+btnStart.addEventListener("click", () => {
+    timer = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStart.disabled = true;
+});
+
+btnStop.addEventListener("click", () => {
+    clearInterval(timer);
+    btnStart.disabled = false;
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
